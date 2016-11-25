a1245fe94184dde7bd1b24edc0a89654ecaf53e3
William Gardiner <bgard017@uottawa.ca>
2016-11-25 12:13:04 -0500

    Classes added

---
+++ app/src/main/java/wtmpd/cookaid/Ingredient.java
@@ -0,0 +1,8 @@
+package wtmpd.cookaid;
+
+/**
+ * Created by wrbg9 on 2016-11-25.
+ */
+
+public class Ingredient {
+}
---
+++ app/src/main/java/wtmpd/cookaid/Recipe.java
@@ -0,0 +1,8 @@
+package wtmpd.cookaid;
+
+/**
+ * Created by William Gardiner on 2016-11-25.
+ */
+
+public class Recipe {
+}
