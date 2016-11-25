54c7019ed42b213b2f3b1606762f8c31a16fa459
Manit Ginoya <ginoya.manit@gmail.com>
2016-11-25 11:54:34 -0500

    Added all Activities for app (No functionality)

---
+++ app/src/main/java/wtmpd/cookaid/RecipeEditActivity.java
@@ -0,0 +1,13 @@
+package wtmpd.cookaid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_edit);
+    }
+}
