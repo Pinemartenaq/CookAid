e6e53beb4f1f033a74514281b31b68d27917e445
Manit Ginoya <ginoya.manit@gmail.com>
2016-11-25 22:53:58 -0500

    MainActivity, InstructionActivity, RecipeSearchActivity and RecipeCreationActivity all now implement the fragment for the navigation bar.

--- app/src/main/java/wtmpd/cookaid/MainActivity.java
+++ app/src/main/java/wtmpd/cookaid/MainActivity.java
@@ -1,13 +1,37 @@
 package wtmpd.cookaid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationBar.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onHomeClick() {
+
+    }
+
+    @Override
+    public void onSearchClick() {
+        Intent intent = new Intent(getApplicationContext(), RecipeSearchActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    public void onAddClick() {
+        Intent intent = new Intent(getApplicationContext(), RecipeCreationActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    public void onInstructionClick() {
+        Intent intent = new Intent(getApplicationContext(), InstructionActivity.class);
+        startActivityForResult(intent, 0);
     }
 }
--- app/src/main/java/wtmpd/cookaid/RecipeCreationActivity.java
+++ app/src/main/java/wtmpd/cookaid/RecipeCreationActivity.java
@@ -1,11 +1,12 @@
 package wtmpd.cookaid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.util.List;
 
-public class RecipeCreationActivity extends AppCompatActivity {
+public class RecipeCreationActivity extends AppCompatActivity implements NavigationBar.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +19,28 @@ public class RecipeCreationActivity extends AppCompatActivity {
         SingletonStorage storage = SingletonStorage.getInstance();
         Recipe newRecipe = new Recipe(name, prepTime, cuisine, type, ingredients, cookingDirections);
         storage.addRecipe(newRecipe);
+    }
+
+    @Override
+    public void onHomeClick() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    public void onSearchClick() {
+        Intent intent = new Intent(getApplicationContext(), RecipeSearchActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    public void onAddClick() {
+
+    }
+
+    @Override
+    public void onInstructionClick() {
+        Intent intent = new Intent(getApplicationContext(), InstructionActivity.class);
+        startActivityForResult(intent, 0);
     }
 }
