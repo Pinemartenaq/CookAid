daf03efca61bd4936cb66034ef6dbc4b0e7df717
William Gardiner <bgard017@uottawa.ca>
2016-11-25 20:23:20 -0500

    Updated Recipe Creation and Recipe Search

--- app/src/main/java/wtmpd/cookaid/RecipeCreationActivity.java
+++ app/src/main/java/wtmpd/cookaid/RecipeCreationActivity.java
@@ -3,11 +3,20 @@ package wtmpd.cookaid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 public class RecipeCreationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_edit);
+    }
+
+    //Private Methods
+    private void addRecipe( String name, int prepTime, RecipeCuisine cuisine, RecipeType type, List<SpecificIngredient> ingredients, String cookingDirections) {
+        SingletonStorage storage = SingletonStorage.getInstance();
+        Recipe newRecipe = new Recipe(name, prepTime, cuisine, type, ingredients, cookingDirections);
+        storage.addRecipe(newRecipe);
     }
 }
